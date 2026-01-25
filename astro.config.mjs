589a352087ee73a022520efacbddebbba05ef216
Clement Bazuaye <clement.bazuaye@interswitchgroup.com>
2026-01-25 07:25:11 +0100

    feat: update school card component to use astro-preload for image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,16 @@ import tailwindcss from "@tailwindcss/vite";
 import netlify from "@astrojs/netlify";
 
 import react from "@astrojs/react";
+import preload from "astro-preload";
 
 import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
+	vite: {
+		plugins: [tailwindcss()],
+	},
 
-    adapter: netlify(),
-    integrations: [react(), compressor()],
+	adapter: netlify(),
+	integrations: [react(), preload(), compressor()],
 });
