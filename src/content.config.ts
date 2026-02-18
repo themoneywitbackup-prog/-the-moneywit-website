2ed7efda4a57fd795c26d0fa0eb7adf611a685d8
mujeeb <shola.mujeeb@gmail.com>
2026-02-18 15:35:10 +0100

    Add more fields

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,21 +1,39 @@
 import {
-	communityCollection,
-	mediaCollection,
-	programCollection,
-	resourceCollection,
-	schoolCollection,
-	
-	
-} from "@/data/content";
-import { campaignPage } from "./data/campaign-page";
-import {  HomepageCollections } from "./data/webpages";
+  communityCollection,
+  mediaCollection,
+  programCollection,
+  resourceCollection,
+  schoolCollection,
+} from '@/data/content'
+import { campaignPage } from './data/campaign-page'
+import {
+  HomepageCollections,
+  whatWeDoPageCollections,
+  schoolPageCollections,
+  mediaPageCollections,
+  communityPageCollections,
+  faqPageCollections,
+  individualPageCollections,
+  corporatePageCollections,
+  aboutPageCollections,
+  customerTestimonialPageCollections
+} from './data/webpages'
 
 export const collections = {
-	schoolCollection,
-	communityCollection,
-	resourceCollection,
-	programCollection,
-	mediaCollection,
-	campaignPage,
-	 HomepageCollections
-};
+  schoolCollection,
+  communityCollection,
+  resourceCollection,
+  programCollection,
+  mediaCollection,
+  campaignPage,
+  HomepageCollections,
+  whatWeDoPageCollections,
+  schoolPageCollections,
+  mediaPageCollections,
+  communityPageCollections,
+  faqPageCollections,
+  individualPageCollections,
+  corporatePageCollections,
+  aboutPageCollections,
+  customerTestimonialPageCollections
+}
